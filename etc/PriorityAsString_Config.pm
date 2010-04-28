80d5ac0ef96f0735caf9da5f21c135b4376fbc62
Christian Loos <chr.loos@googlemail.com>
2010-04-28 23:46:00 +0200

    adding priority None

--- etc/PriorityAsString_Config.pm
+++ etc/PriorityAsString_Config.pm
@@ -1,7 +1,7 @@
 # RT::Extension::PriorityAsString configuration
 
-Set(%PriorityAsString, (Low => 0, Medium => 50, High => 100));
+Set(%PriorityAsString, (None => 0, Low => 1, Medium => 50, High => 100));
 
-Set(@PriorityAsStringOrder, qw(Low Medium High));
+Set(@PriorityAsStringOrder, qw(None Low Medium High));
 
 1;
