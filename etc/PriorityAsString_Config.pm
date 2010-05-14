92d63ba0d0cea613145561951986ed7c51926020
Christian Loos <cloos@netsandbox.de>
2010-05-14 23:38:29 +0200

    changed priority number to string mapping
    using 25 for low and 75 for high to leave space for
    lower than low and higher than high (if this will be needed)

--- etc/PriorityAsString_Config.pm
+++ etc/PriorityAsString_Config.pm
@@ -1,6 +1,6 @@
 # RT::Extension::PriorityAsString configuration
 
-Set(%PriorityAsString, (None => 0, Low => 1, Medium => 50, High => 100));
+Set(%PriorityAsString, (None => 0, Low => 25, Medium => 50, High => 75));
 
 Set(@PriorityAsStringOrder, qw(None Low Medium High));
 
