15972ce9ed91ebc0dbcd12d4b2fb6f078bdd8710
Christian Loos <cloos@netcologne.de>
2013-03-12 10:26:10 +0100

    fix indentation

--- etc/PriorityAsString_Config.pm
+++ etc/PriorityAsString_Config.pm
@@ -1,10 +1,10 @@
 # RT::Extension::PriorityAsString configuration
 
 Set(%PriorityAsString, (
-    None => 0, #loc_left_pair
-    Normal => 25, #loc_left_pair
-    Medium => 50, #loc_left_pair
-    High => 75, #loc_left_pair
+    None       => 0, #loc_left_pair
+    Normal     => 25, #loc_left_pair
+    Medium     => 50, #loc_left_pair
+    High       => 75, #loc_left_pair
     Escalation => 99) #loc_left_pair
 );
 
